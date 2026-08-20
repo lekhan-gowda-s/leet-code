e487a442a94821d541ff1a3d99f46666b2c21b24
lekhan gowda s <lkgs0129@gmail.com>
2026-08-21 01:05:15 +0530

    Time: 23 ms (53.76%) | Memory: 81.2 MB (79.31%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,26 +1,26 @@
-class Solution {
+class Solution
+{
 public:
-    int largestRectangleArea(vector<int>& arr) 
+    int largestRectangleArea(vector<int> &arr)
     {
         stack<int> st;
-        int ma=INT_MIN;
-        for(int i=0;i<arr.size();i++)
+        int ma = INT_MIN;
+        for (int i = 0; i < arr.size(); i++)
         {
-            while(!st.empty() && arr[st.top()] > arr[i])
-    {
-    int el = st.top();
-    st.pop();
-
-    if(st.empty())
-    {
-        ma = max(ma, arr[el] * i);
-    }
-    else
-    {
-        ma = max(ma, arr[el] * (i - st.top() - 1));
-    }
-}
-            if(st.empty())
+            while (!st.empty() && arr[st.top()] > arr[i])
+            {
+                int el = st.top();
+                st.pop();
+                if (st.empty())
+                {
+                    ma = max(ma, arr[el] * i);
+                }
+                else
+                {
+                    ma = max(ma, arr[el] * (i - st.top() - 1));
+                }
+            }
+            if (st.empty())
             {
                 st.push(i);
             }
@@ -29,17 +29,17 @@ public:
                 st.push(i);
             }
         }
-        while(!st.empty())
+        while (!st.empty())
         {
-            int elem=arr[st.top()];
+            int elem = arr[st.top()];
             st.pop();
-            if(st.empty())
+            if (st.empty())
             {
                 ma = max(ma, elem * (int)arr.size());
             }
             else
             {
-                ma=max(ma,elem*(int)(arr.size()-st.top()-1));
+                ma = max(ma, elem * (int)(arr.size() - st.top() - 1));
             }
         }
         return ma;
